b7609c82669bbbe5dab3a420728cab50866a9259
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-05-12 00:03:04 +0530

    Update resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="#"
+            href="https://drive.google.com/file/d/1gau5zFeKHeISbF0kdDu_Q05dgMXx-iXa/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
